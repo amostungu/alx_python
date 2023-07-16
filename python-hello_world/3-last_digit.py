a33809b9941943634c2cda68cdd9b56f2f0073e1
amostungu <amostungu1@gmail.com>
2023-07-16 02:33:50 -0700

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit = (number) % 10
+last_digit = abs(number) % 10
 if number < 0:
     last_digit = -last_digit
     print("Last digit of {} is {}".format(number, last_digit), end="")
