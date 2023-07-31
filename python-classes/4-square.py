a451de9192b89bdd0db92fe4cd2bae1a9a27711c
amostungu <amostungu1@gmail.com>
2023-07-31 13:08:02 -0700

    Printing a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,12 +8,12 @@ class Square:
     def __init__(self, size=0):
         """initializing a new square."""
         self.size = size
-    
+
     @property
     def size(self):
         """set the current size of square"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):  
         if not isinstance(value, int):
@@ -21,11 +21,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")    
         self.__size = value
-        
+
     def area(self):
         """Return the current area of square."""
         return self.__size ** 2
-    
+
     def my_print(self):
         """printing output"""
         if self.__size == 0:
