b72c24f3bc2154722e9478fe6f6b328f5669a076
amostungu <amostungu1@gmail.com>
2023-08-06 03:01:06 -0700

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -10,9 +10,12 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """initialization"""
-
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def integer_validator(self, name, value):
         """integrator validator"""
