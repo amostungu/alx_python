5a58f091c261eeecdb7a4f31e93f8b948acfcd22
amostungu <amostungu1@gmail.com>
2023-08-05 22:11:19 -0700

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """Defines an empty class BaseGeometry."""
-
+class baseGeo(type):
+    """The type class"""
+    def __dir__(cls):
+        """inharitance from type class"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
 class BaseGeometry:
     """basegeometry class"""
-
-    pass
+    def __dir__(cls):
+        """class inheritance from type"""
+        return [attribute for attribute in super().__dir__() if attribute !='__init_subclass__']
