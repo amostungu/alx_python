dcee1916c5379810d34dcf6522995efc0618587e
amostungu <amostungu1@gmail.com>
2023-08-06 03:09:14 -0700

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -18,6 +18,16 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def integer_validator(self, name, value):
-        """integrator validator"""
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError(f"{name} must be a positive integer.")
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
