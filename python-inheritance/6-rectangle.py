21e05126584f18e001377cecfa71071465b8feb6
amostungu <amostungu1@gmail.com>
2023-08-06 02:24:19 -0700

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
 
+
 BaseGeometry = import('5-base_geometry.py').BaseGeometry
+"""importing"""
 
 class Rectangle(BaseGeometry):
     """Rectangle inheritance"""
+
     def __init__(self, width, height):
+        """initialization"""
         self.__width = super().integer_validator("width", width)
         self.__height = super().integer_validator("height", height)
