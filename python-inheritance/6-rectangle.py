8cd2cb6ca61c63e8ca0cffbff93e3005fe473ddc
amostungu <amostungu1@gmail.com>
2023-08-06 03:18:09 -0700

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
 
+class baseGeo(type):
+    """The custom metaclass"""
+    def __dir__(cls):
+        """Overrides dir() method for the metaclass"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
-BaseGeometry = __import__('5-base_geometry.py').BaseGeometry
-"""importing"""
+class BaseGeometry:
+    """Reprsent base geometry."""
 
-class Rectangle(BaseGeometry):
-    """Rectangle inheritance"""
-
-    def __init__(self, width, height):
-        """initialization"""
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
@@ -31,3 +28,10 @@ class Rectangle(BaseGeometry):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+    def __dir__(cls):
+        """Overrides dir() method for the metaclass"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
