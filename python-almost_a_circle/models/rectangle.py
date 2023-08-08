72c1e3ef863ec65a7752e89053b48fa60d25518f
amostungu <amostungu1@gmail.com>
2023-08-08 11:02:50 -0700

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -45,6 +45,10 @@ class Rectangle(Base):
         Returns:
             None.
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -65,9 +69,14 @@ class Rectangle(Base):
         Args:
             value (int): The desired height value.
 
-        Returns:
-            None.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
    
     @property
@@ -88,9 +97,14 @@ class Rectangle(Base):
         Args:
             value (int): The desired x-coordinate value.
 
-        Returns:
-            None.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
  
     @property
@@ -111,7 +125,12 @@ class Rectangle(Base):
         Args:
             value (int): The desired y-coordinate value.
 
-        Returns:
-            None.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
