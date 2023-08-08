00c098eabf11e431901934280b1052058586727d
amostungu <amostungu1@gmail.com>
2023-08-08 12:26:50 -0700

    Update #0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -156,3 +156,18 @@ class Rectangle(Base):
         Return a string representation of the Rectangle instance.
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """
+        Update attributes of the rectangle based on no-keyword arguments.
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
