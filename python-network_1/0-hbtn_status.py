aaa8ea3ce76e68ddf1f59975f6a58ae31e7ff7b7
amostungu <amostungu1@gmail.com>
2023-08-09 12:21:31 -0700

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -13,17 +13,14 @@ response = requests.get(url)
 """Send a GET request to the URL"""
 
 if response.status_code == 200:
-    try:
-        # Try to parse the JSON content of the response
-        data = response.json()
-        # Display the formatted response body
+    if response.text == "OK":
+        # Display the text content of the response
         print("Body response:")
-        print("\t- type:", type(data))     # Display the type of the data (dictionary in this case)
-        print("\t- content:", data)        # Display the content of the response JSON data
-    except Exception as e:
-        # If JSON parsing fails, display the raw content of the response
-        print("Failed to parse JSON. Raw content:")
-        print(response.text)
+        print("\t- type: str")
+        print("\t- content:", response.text)
+    else:
+        # Display an error message if the response content is unexpected
+        print("Unexpected response content:", response.text)
 else:
     # Display an error message if the status code is not 200
     print(f"Error: {response.status_code}")
