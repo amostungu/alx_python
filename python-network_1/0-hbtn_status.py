d0beac102cc92bf0cfaeb0ba86759acb6a684504
amostungu <amostungu1@gmail.com>
2023-08-09 12:09:07 -0700

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,21 +5,24 @@
 import requests
 """Importing requests"""
 
-# URL to fetch data from
+
 url = "https://alu-intranet.hbtn.io/status"
+"""URL to fetch data from"""
 
-# Send a GET request to the URL
 response = requests.get(url)
+"""Send a GET request to the URL"""
 
-# Check if the response status code is 200 (OK)
 if response.status_code == 200:
-    # Parse the JSON content of the response
+    """Check if the response status code is 200 (OK)"""
     data = response.json()
+    """Parse the JSON content of the response"""
 
-    # Display the formatted response body
     print("Body response:")
+    """Display the formatted response body"""
     print("\t- type:", type(data))
+    """Display the type of the data (dictionary in this case)"""
     print("\t- content:", data)
+    """Display the content of the response JSON data"""
 else:
-    # Display an error message if the status code is not 200
+    """Display an error message if the status code is not 200"""
     print(f"Error: {response.status_code}")
