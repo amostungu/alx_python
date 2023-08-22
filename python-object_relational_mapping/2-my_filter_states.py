9c3966f35d49ecd1439cdec9fc143e819974665a
amostungu <amostungu1@gmail.com>
2023-08-22 13:11:09 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches"""
+"""a script that takes in an argument and displays all values in states table of hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 # The code should not be executed when imported
 if __name__ == "__main__":
-    
+
     # make a connection to the database
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
