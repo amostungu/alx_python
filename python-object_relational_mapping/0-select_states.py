dd38905537de408b5287244d9760b0bbe844ef4c
amostungu <amostungu1@gmail.com>
2023-08-22 12:24:55 -0700

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
     # It gives us the ability to have multiple seperate working environments
     cur = db.cursor()
