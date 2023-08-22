14467ad9606307b86b69db75afeb90a0c9c79ea9
amostungu <amostungu1@gmail.com>
2023-08-22 11:36:22 -0700

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 import MySQLdb
 #importing MySQL
-import sys
-#importing sys
-db=MySQLdb.connect(host="localhost", port=3306, password="password", database="hbtn_0e_0_usa")
+
+db=MySQLdb.connect(host="localhost", port=3306, user="root" password="root", db="hbtn_0e_0_usa" charset="utf8")
 cursor = db.cursor()
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
 #Execute the query to fetch states and sort by states.id
 query = "SELECT * FROM states ORDER BY id ASC"
 cursor.execute(query)
