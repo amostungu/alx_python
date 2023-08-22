2cd412c9a9cf0d94de451fac0b061a153ca7a8bb
amostungu <amostungu1@gmail.com>
2023-08-22 13:07:37 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 """a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches"""
+import sys
 import MySQLdb
-from sys import argv
 
 # The code should not be executed when imported
-if __name__ == '__main__':
-
+if __name__ == "__main__":
+    
     # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     # It gives us the ability to have multiple seperate working environments
-    cur = db.cursor()
-    cur.execute("SELECT * FROM statesWHERE name LIKE BINARY '{}'".format(argv[4]))
-
-    rows = cur.fetchall()
-    for j in rows:
-        print(j)
-    # Clean up process
-    cur.close()
-    db.close()
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
