b7df40b55b5d2b4934e8413d9a9b716dfc667f9e
amostungu <amostungu1@gmail.com>
2023-08-24 11:46:41 -0700

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,11 +14,9 @@ Base = declarative_base()
 class City(Base):
     """Represents a city for a MySQL database.
     Attributes:
-        id (str): The city's id.
-        name (sqlalchemy.Integer): The city's name.
-        state_id (sqlalchemy.String): The city's state id.
+        id (str): The state's id.
+        name (sqlalchemy.Integer): The state's name.
     """
-    __tablename__ = "cities"
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
