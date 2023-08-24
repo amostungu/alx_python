af8325a32b883fff2a9adb3cc83d8f46b35e48b5
amostungu <amostungu1@gmail.com>
2023-08-24 09:43:45 -0700

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Class definition of a City"""
 
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 """Importing Column, Integer, String and Foreignkey from sqlalchemy"""
+
 from model_state import Base
 """Importing base from model state"""
 
@@ -10,9 +12,14 @@ class City(Base):
     """City class that inherits from Base"""
 
     __tablename__ = "cities"
+    """Database table"""
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     """An auto-incrementing primary key for the City table."""
+
     name = Column(String(128), nullable=False)
     """The name of the city, limited to 128 characters."""
+
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
     """A foreign key referencing the id column of the associated state."""
