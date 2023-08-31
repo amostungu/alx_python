4cf34896283dce15fe49e57d6754fdfa7492d905
amostungu <amostungu1@gmail.com>
2023-08-31 11:41:11 -0700

    Number template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -54,10 +54,10 @@ def display_number(n):
     return f"{n} is a number"
 
 #Route /number_template/<n>.
-@app.route('/number_template/<int: n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def template_number(n):
     # Route handler for /number_template/<n>.
-    return render_template("templates/5-number.html", number=n)
+    return render_template("5-number.html", number=n)
 
 
 # Start the Flask application only when this script is run directly
