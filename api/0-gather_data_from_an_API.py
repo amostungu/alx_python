00a774867e9f6369d46c787acc284afd29ecebb7
amostungu <amostungu1@gmail.com>
2023-10-03 11:28:12 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,26 +3,26 @@ import sys
 
 def get_employee_data(employee_id):
     """Defining the API endpoint for employee"""
-    employee_url =  f"https://jsonplaceholder.typicode.com/users/1"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/1/todos"
+    employee_url = request.get('https://jsonplaceholder.typicode.com/users/{employee_id}')
+    todo_url = request.get('https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
     try:
         #Getting the employee details
         employee_response = requests.get(employee_url)
         employee_data = employee_response.json()
-        employee_name = employee_data.get("name")
+        employee_name = employee_data.get('name')
 
         #Getting Todo list
         todo_response = requests.get(todo_url)
         todo_list = todo_response.json()
 
         #Getting number of completed items
-        completed_tasks = [task for task in todo_list if task["completed"]]
+        completed_tasks = [task for task in todo_list if task['completed']]
         total_tasks = len(todo_list)
         num_completed = len(completed_tasks)
 
         #Get output results
-        print(f"Employee {employee_name} is done with tasks({num_completed}/{total_tasks}):")
+        print(f'Employee {employee_name} is done with tasks({num_completed}/{total_tasks}):')
         for task in completed_tasks:
             print(f"\t{task['title']}")
 
