5845a3588e7e3c0857a9e90547d0b453144a938c
amostungu <amostungu1@gmail.com>
2023-10-04 11:04:17 -0700

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,98 +1,59 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
-for a given employee ID, returns information about
-his/her TODO list progress and exports it in CSV format.
+Check student .CSV output of user information and export tasks in CSV format
 """
 
 import csv
 import requests
 import sys
 
-def get_employee_data(employee_id):
-    # Define the base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com"
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    # Construct the URLs for employee details and TODO list
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
 
-    # Fetch employee details
-    try:
-        response = requests.get(employee_url)
-        response.raise_for_status()
-        employee_data = response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching employee details: {e}")
-        sys.exit(1)
+def export_tasks_to_csv(id, tasks):
+    """ Export tasks to CSV format """
+    filename = f"{id}.csv"
 
-    # Fetch TODO list
-    try:
-        response = requests.get(todo_url)
-        response.raise_for_status()
-        todo_data = response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching TODO list: {e}")
-        sys.exit(1)
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    return employee_data, todo_data
+        writer.writeheader()  # Write CSV header
 
-def export_to_csv(employee_id, employee_name, todo_data):
-    # Define the CSV file name
-    csv_filename = f"{employee_id}.csv"
+        for task in tasks:
+            # Retrieve the username for the given user ID from the API
+            response = requests.get(users_url + str(id)).json()
+            username = response[0]['username']
 
-    # Write tasks to CSV file
-    with open(csv_filename, mode='w', newline='') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            # Write task data to CSV
+            writer.writerow({
+                "USER_ID": id,
+                "USERNAME": username,
+                "TASK_COMPLETED_STATUS": str(task['completed']),
+                "TASK_TITLE": task['title']
+            })
 
-        # Write header row
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    print(f"Tasks exported to {filename}")
 
-        # Write tasks
-        for task in todo_data:
-            writer.writerow([employee_id, employee_name, task["completed"], task["title"]])
 
-def display_todo_progress(employee_name, todo_data):
-    completed_tasks = [task for task in todo_data if task["completed"]]
-    total_tasks = len(todo_data)
+def user_info(id):
+    """ Check user information and export tasks to CSV """
 
-    # Display employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+    # Fetch tasks for the given user ID from the API
+    tasks = [task for task in requests.get(todos_url).json() if task['userId'] == id]
 
-def check_number_of_tasks_in_csv(employee_id, todo_data):
-    csv_filename = f"{employee_id}.csv"
+    # Export tasks to CSV
+    export_tasks_to_csv(id, tasks)
 
-    # Count the total number of tasks
-    total_tasks = len(todo_data)
-
-    # Count the number of lines in the CSV file
-    num_lines = 0
-    with open(csv_filename, 'r') as f:
-        for line in f:
-            if not line == '\n':
-                num_lines += 1
-
-    if total_tasks == num_lines:
-        print("Number of tasks in CSV: OK")
-    else:
-        print("Number of tasks in CSV: Incorrect")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python gather_data_and_export.py <employee_id>")
+        print("Usage: python check_student_output.py <employee_id>")
         sys.exit(1)
 
     try:
-        employee_id = int(sys.argv[1])
+        user_info(int(sys.argv[1]))
     except ValueError:
         print("Employee ID must be an integer.")
         sys.exit(1)
-
-    employee_data, todo_data = get_employee_data(employee_id)
-    employee_name = employee_data.get("name")
-    
-    export_to_csv(employee_id, employee_name, todo_data)
-    display_todo_progress(employee_name, todo_data)
-    check_number_of_tasks_in_csv(employee_id, todo_data)
